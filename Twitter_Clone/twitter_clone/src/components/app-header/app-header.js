0b106e984eee3656aa888a557b9869282805742d
Kirill <balanovichks@gmail.com>
2020-09-01 00:07:02 +0300

    components with styles add to the project

---
+++ Twitter_Clone/twitter_clone/src/components/app-header/app-header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './app-header.css';
+const AppHeader = () => {
+    return (
+        <div className="app-header d-flex">
+            <h1>Kirill Balanovich</h1>
+            <h2>5 tweets, liked 0</h2>
+        </div>
+    )
+}
+
+export default AppHeader;
